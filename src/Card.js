b258abffbb38e76f0fb2aa4e70d7edc1a9ffe7e2
Russell Hart <russellbot@gmail.com>
2020-11-23 14:59:15 -0500

    props in card component

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './Card.css'
 
-const Card = () => {
+const Card = (props) => {
+    const { name, explanation, date } = props;
     return (
         <div className='card'>
             <img alt='nasapicture' src='https://robohash.org/test?200x200' />
             <div>
-                <h2>Moon Pic</h2>
-                <p>blah blah blah</p>
+                <h2>{name}</h2>
+                <p>{explanation}</p>
+                <p>{date}</p>
             </div>
         </div>
     );
