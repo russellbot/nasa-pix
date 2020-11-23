543fef0afc2290b71d18b4998300ad5b7c2ed9fb
Russell Hart <russellbot@gmail.com>
2020-11-22 22:10:09 -0500

    card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className='card'>
+            <img alt='photo' src='https://robohash.org/test?200x200' />
+            <div>
+                <h2>Moon Pic</h2>
+                <p>blah blah blah</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
