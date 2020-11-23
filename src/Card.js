d8863ac9bb6ef856159ef8b62a89448b72111222
Russell Hart <russellbot@gmail.com>
2020-11-23 14:48:29 -0500

    added sample list of nasa pics

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = () => {
     return (
         <div className='card'>
-            <img alt='photo' src='https://robohash.org/test?200x200' />
+            <img alt='nasapicture' src='https://robohash.org/test?200x200' />
             <div>
                 <h2>Moon Pic</h2>
                 <p>blah blah blah</p>
---
+++ src/pix.js
@@ -0,0 +1,97 @@
+[
+    {
+    date: "2016-01-17",
+    explanation: "The center of our Galaxy is a busy place. In visible light, much of the Galactic Center is obscured by opaque dust. In infrared light, however, dust glows more and obscures less, allowing nearly one million stars to be recorded in the featured photograph. The Galactic Center itself appears on the left and is located about 30,000 light years away towards the constellation of the Archer (Sagittarius). The Galactic Plane of our Milky Way Galaxy, the plane in which the Sun orbits, is identifiable by the dark diagonal dust lane. The absorbing dust grains are created in the atmospheres of cool red-giant stars and grow in molecular clouds. The region directly surrounding the Galactic Center glows brightly in radio and high-energy radiation, and is thought to house a large black hole. Follow APOD on: Facebook, Google Plus, or Twitter",
+    hdurl: "https://apod.nasa.gov/apod/image/1601/gcenter_2mass_620.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "The Galactic Center in Infrared",
+    url: "https://apod.nasa.gov/apod/image/1601/gcenter_2mass_960.jpg"
+    },
+    {
+    copyright: "Joseph Brimacombe",
+    date: "2008-11-11",
+    explanation: "First cataloged as a star, 30 Doradus is actually an immense star forming region in nearby galaxy The Large Magellanic Cloud. The region's spidery appearance is responsible for its popular name, the Tarantula nebula, except that this tarantula is about 1,000 light-years across, and 180,000 light-years away in the southern constellation Dorado. If the Tarantula nebula were at the distance of the Orion Nebula (1,500 light-years), the nearest stellar nursery to Earth, it would appear to cover about 30 degrees (60 full moons) on the sky. The spindly arms of the Tarantula nebula surround NGC 2070, a star cluster that contains some of the brightest, most massive stars known. Intriguing details of the nebula are visible in this scientifically-colored image. The cosmic Tarantula also lies near the site of the closest recent supernova. digg_url = 'http://apod.nasa.gov/apod/ap081111.html'; digg_skin = 'compact';",
+    hdurl: "https://apod.nasa.gov/apod/image/0811/tarantula_brimacombe_big.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "The Cosmic Web of the Tarantula Nebula",
+    url: "https://apod.nasa.gov/apod/image/0811/tarantula_brimacombe.jpg"
+    },
+    {
+    copyright: "PhotoAstronomique.net",
+    date: "2006-08-19",
+    explanation: "To paraphrase Magritte, "This is not a meteor". It's not a picture of a meteor either, but it was taken during last weekend's peak of the Perseid Meteor shower. Skywatching with friends at a cosy beach campsite bathed in moonlight at Treguennec, France, astronomer and APOD translator Laurent Laveder planned to record bright Perseid meteors with camera and tripod. While the Perseid meteors he saw were neither numerous nor bright he did capture the brilliant trail of an Iridium communication satellite. His long exposure began after the satellite glint became visible, so the resulting streak does resemble a meteor trail in the final image. Also recognizable in the serene view of sandy beach and starry sky is the famous northern asterism, the Big Dipper.",
+    hdurl: "https://apod.nasa.gov/apod/image/0608/notmeteor_laveder_f.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Ceci n'est pas un Meteore",
+    url: "https://apod.nasa.gov/apod/image/0608/notmeteor_laveder_f72.jpg"
+    },
+    {
+    copyright: "Phil Hart",
+    date: "2009-12-18",
+    explanation: "At least 34 meteors are included in this composite image as they rain through Australian skies during the annual Geminid Meteor shower. Dust particles strung out along the orbit of extinct comet Phaethon vaporize when they plow through planet Earth's atmosphere causing the impressive display. Although the particles are traveling parallel to each other, the resulting streaks clearly seem to radiate from a single point on the sky near Gemini's twin stars Castor and Pollux at the lower right. The radiant effect is due to perspective, as the parallel tracks appear to converge at a distance. Taken over a period of 2 hours on the morning of December 14, short exposures recording individual meteor streaks were combined with a single long exposure to show the background stars, with Sirius at the top, and the constellation Orion at left. Faint stars and nebulae of the Milky Way track through the center of the frame. Near the radiant point, an extra star in Gemini is actually the flash of a meteor seen almost head-on. digg_url = 'http://apod.nasa.gov/apod/ap091218.html'; digg_skin = 'compact';",
+    hdurl: "https://apod.nasa.gov/apod/image/0912/GeminidMeteors4hart_1200px.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Southern Geminids",
+    url: "https://apod.nasa.gov/apod/image/0912/GeminidMeteors4hart_900px.jpg"
+    },
+    {
+    date: "1997-02-04",
+    explanation: "Astronomer Clyde Tombaugh, discoverer of Pluto, died on January 17th. Inspiring many during his long and exceptional career, he had been living in Las Cruces, New Mexico with his wife of 60 years, Patsy. Today would have been his 91st birthday. He is pictured above in 1995 in his backyard with a telescope he knew well - a 9 inch Newtonian reflector he built in 1927 with discarded farm machinery and car parts. Using this telescope under the dark night skies of Western Kansas, he made drawings of Mars and Jupiter and submitted them to Lowell Observatory in 1928. Hired to work at Lowell in 1929, Tombaugh embarked on a systematic photographic search for the long sought Planet X with a newly constructed 13 inch astrograph. In 1930 Tombaugh triumphed in his struggle to find the 9th planet, discovering faint and distant Pluto orbiting at the edge of our Solar System. Founding father of New Mexico State University's Astronomy Department, he retired as professor emeritus in 1973 but continued to tour as a lecturer and promoter until failing health prevented it. Always an active stargazer, he was asked by the Smithsonian if they could have the telescope he used to make his 1928 drawings. His response: "I told them I was still using it."",
+    hdurl: "https://apod.nasa.gov/apod/image/9702/tombaugh_beatty.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Clyde W. Tombaugh: 1906-1997 Credit and Copyright:",
+    url: "https://apod.nasa.gov/apod/image/9702/tombaugh_beatty.jpg"
+    },
+    {
+    copyright: "Roberto Colombari",
+    date: "2013-10-29",
+    explanation: "The dark Horsehead Nebula and the glowing Orion Nebula are contrasting cosmic vistas. Adrift 1,500 light-years away in one of the night sky's most recognizable constellations, they appear in opposite corners of the above stunning mosaic. The familiar Horsehead nebula appears as a dark cloud, a small silhouette notched against the long red glow at the lower left. Alnitak is the easternmost star in Orion's belt and is seen as the brightest star to the left of the Horsehead. Below Alnitak is the Flame Nebula, with clouds of bright emission and dramatic dark dust lanes. The magnificent emission region, the Orion Nebula (aka M42), lies at the upper right. Immediately to its left is a prominent reflection nebula sometimes called the Running Man. Pervasive tendrils of glowing hydrogen gas are easily traced throughout the region.",
+    hdurl: "https://apod.nasa.gov/apod/image/1310/orionwisps_colombari_3543.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Horsehead and Orion Nebulas",
+    url: "https://apod.nasa.gov/apod/image/1310/orionwisps_colombari_960.jpg"
+    },
+    {
+    copyright: "Jean-Luc Dauvergne",
+    date: "2010-02-05",
+    explanation: "It's spring for the northern hemisphere of Mars and spring on Mars usually means dust storms. So the dramatic brown swath of dust (top) marking the otherwise white north polar cap in this picture of the Red Planet is not really surprising. Taking advantage of the good views of Mars currently possible near opposition and its closest approach to planet Earth in 2010, this sharp image shows the evolving dust storm extending from the large dark region known as Mare Acidalium below the polar cap. It was recorded on February 2nd with the 1 meter telescope at Pic Du Midi, a mountain top observatory in the French Pyrenees. digg_url = 'http://apod.nasa.gov/apod/ap100205.html'; digg_skin = 'compact';",
+    hdurl: "https://apod.nasa.gov/apod/image/1002/MarsDust_picdumidi.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Dust Storm on Mars",
+    url: "https://apod.nasa.gov/apod/image/1002/MarsDust_picdumidi_c740.jpg"
+    },
+    {
+    date: "2002-12-28",
+    explanation: "This dream-like image of Mir was recorded by astronauts as the Space Shuttle Atlantis approached the Russian space station prior to docking during the STS-76 mission. Sporting spindly appendages and solar panels, Mir resembles a whimsical flying insect hovering about 350 kilometers above New Zealand's South Island and the city of Nelson near Cook Strait. In late March 1996, Atlantis shuttled astronaut Shannon W. Lucid to Mir for a five month visit, increasing Mir's occupancy from 2 to 3. It returned to pick Lucid up and drop off astronaut John Blaha during the STS-79 mission in August of that year. Since becoming operational in 1986, Mir has been visited by over 100 spacefarers from the nations of planet Earth including, Russia, the United States, Great Britain, Germany, France, Japan, Austria, Kazakhstan and Slovakia. After joint Shuttle-Mir training missions in support of the International Space Station, continuous occupation of Mir ended in August 1999. The Mir was deorbited in March 2001.",
+    hdurl: "https://apod.nasa.gov/apod/image/0212/mirdream_sts76_big.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Mir Dreams",
+    url: "https://apod.nasa.gov/apod/image/0212/mirdream_sts76.jpg"
+    },
+    {
+    date: "2004-09-06",
+    explanation: "A comet-like tail of glowing gas, 200,000 light-years long, streams from galaxy C153 as it plunges through galaxy cluster Abell 2125 at nearly 8 million kilometers per hour. Itself a member of the giant cluster of galaxies, C153 may once have been a spiral galaxy like the Milky Way. But this remarkable series of images, false-color composites of x-ray and optical data, zooms in on the galaxy's fate. A headlong passage through the hot intracluster gas in the central regions of Abell 2125 is seen to be stripping C153 of its own star forming material and distorting its shape. As other galaxies in the cluster suffer a similar fate, the hot gas collecting in the cluster's core should become enriched in heavy elements. The violent spectacle was taking place about 3 billion light-years from Earth and is thought to illustrate a common process in the cosmic evolution of large clusters of galaxies.",
+    hdurl: "https://apod.nasa.gov/apod/image/0409/a2125_comp_3panel_labeled.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "C153 Takes the Plunge",
+    url: "https://apod.nasa.gov/apod/image/0409/a2125_comp_3panel_labsm1.jpg"
+    },
+    {
+    date: "2004-06-20",
+    explanation: "Season's greetings! Today or tomorrow, depending on your time zone, the Sun reaches its northernmost point in planet Earth's sky marking a season change and the first solstice of the year 2004. In celebration, consider this delightfully detailed, brightly colored image of the active Sun. From the EIT instrument onboard the space-based SOHO observatory, the tantalizing picture is a false-color composite of three images all made in extreme ultraviolet light. Each individual image highlights a different temperature regime in the upper solar atmosphere and was assigned a specific color; red at 2 million, green at 1.5 million, and blue at 1 million degrees C. The combined image shows bright active regions strewn across the solar disk, which would otherwise appear as dark groups of sunspots in visible light images, along with some magnificent plasma loops and an immense prominence at the right hand solar limb.",
+    hdurl: "https://apod.nasa.gov/apod/image/0212/sun3col_thompsoneit_big.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Solstice Celebration",
+    url: "https://apod.nasa.gov/apod/image/0212/sun3col_thompsoneit.jpg"
+    }
+]
