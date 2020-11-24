c58237ce0491680a697d59b8b4e19d5f6e0de879
Russell Hart <russellbot@gmail.com>
2020-11-24 10:57:40 -0500

    added css to carlist

--- src/CardList.js
+++ src/CardList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Card from './Card';
+import './CardList.css'
 
 const CardList = ({ pix }) => {
     return (
-        <div>
+        <div className='images-container'>
             <Card title={pix[0].title} url={pix[0].url} explanation={pix[0].explanation} date={pix[0].date} copyright={pix[0].copyright} />
             <Card title={pix[1].title} url={pix[1].url} explanation={pix[1].explanation} date={pix[1].date} copyright={pix[1].copyright} />
             <Card title={pix[2].title} url={pix[2].url} explanation={pix[2].explanation} date={pix[2].date} copyright={pix[2].copyright} />
