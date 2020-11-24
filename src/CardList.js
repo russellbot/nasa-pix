2399b883714e7a079f045975505c53955f9cf01f
Russell Hart <russellbot@gmail.com>
2020-11-24 14:29:04 -0500

    added cardComponent function

--- src/CardList.js
+++ src/CardList.js
@@ -3,12 +3,12 @@ import Card from './Card';
 import './CardList.css'
 
 const CardList = ({ pix }) => {
+    const cardComponent = pix.map((picture, i) => {
+        return <Card title={pix[i].title} url={pix[i].url} explanation={pix[i].explanation} date={pix[i].date} copyright={pix[i].copyright} />
+    })
     return (
         <div className='images-container'>
-            <Card title={pix[0].title} url={pix[0].url} explanation={pix[0].explanation} date={pix[0].date} copyright={pix[0].copyright} />
-            <Card title={pix[1].title} url={pix[1].url} explanation={pix[1].explanation} date={pix[1].date} copyright={pix[1].copyright} />
-            <Card title={pix[2].title} url={pix[2].url} explanation={pix[2].explanation} date={pix[2].date} copyright={pix[2].copyright} />
-            <Card title={pix[4].title} url={pix[4].url} explanation={pix[4].explanation} date={pix[4].date} copyright={pix[4].copyright} />
+            {cardComponent}
         </div>
     );
 }
