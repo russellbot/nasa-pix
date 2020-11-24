401cc56c855d0b28b77874749e293abfdd667b7e
Russell Hart <russellbot@gmail.com>
2020-11-24 14:35:09 -0500

    added anchor tag

--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './Card.css'
 
 const Card = (props) => {
-    const { title, explanation, date, url, copyright } = props;
+    const { title, explanation, date, url, copyright, hdurl } = props;
     return (
         <div className='card'>
-            <img alt='nasapicture' src={url} />
+            <a href={hdurl}>
+                <img alt='nasapicture' src={url} />
+            </a>            
             <div>
                 <h2>{title}</h2>
                 <p>{explanation}</p>
