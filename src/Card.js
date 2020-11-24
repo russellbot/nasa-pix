5f0428fca922bff7537c176f359f30cdd74cd04a
Russell Hart <russellbot@gmail.com>
2020-11-24 14:25:16 -0500

    added copyright

--- src/Card.js
+++ src/Card.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './Card.css'
 
 const Card = (props) => {
-    const { title, explanation, date, url } = props;
+    const { title, explanation, date, url, copyright } = props;
     return (
         <div className='card'>
             <img alt='nasapicture' src={url} />
             <div>
                 <h2>{title}</h2>
                 <p>{explanation}</p>
-                <p>{date}</p>
+                <div className='card-bottom'>
+                    <p>{date}</p>
+                    <p>{copyright}</p>
+                </div>
             </div>
         </div>
     );
