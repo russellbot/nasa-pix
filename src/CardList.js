e16efaaf735dd8dddce7562740c843779d6a13e5
Russell Hart <russellbot@gmail.com>
2020-11-24 10:55:08 -0500

    added more cards

--- src/CardList.js
+++ src/CardList.js
@@ -3,7 +3,12 @@ import Card from './Card';
 
 const CardList = ({ pix }) => {
     return (
-        <Card title={pix[0].title} url={pix[0].url} explanation={pix[0].explanation} date={pix[0].date} copyright={pix[0].copyright} />
+        <div>
+            <Card title={pix[0].title} url={pix[0].url} explanation={pix[0].explanation} date={pix[0].date} copyright={pix[0].copyright} />
+            <Card title={pix[1].title} url={pix[1].url} explanation={pix[1].explanation} date={pix[1].date} copyright={pix[1].copyright} />
+            <Card title={pix[2].title} url={pix[2].url} explanation={pix[2].explanation} date={pix[2].date} copyright={pix[2].copyright} />
+            <Card title={pix[4].title} url={pix[4].url} explanation={pix[4].explanation} date={pix[4].date} copyright={pix[4].copyright} />
+        </div>
     );
 }
 
