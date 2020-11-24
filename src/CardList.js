ac16003ee69db852f32dffd8ff868a35d2140031
Russell Hart <russellbot@gmail.com>
2020-11-24 14:31:29 -0500

    added key to card component

--- src/CardList.js
+++ src/CardList.js
@@ -4,7 +4,7 @@ import './CardList.css'
 
 const CardList = ({ pix }) => {
     const cardComponent = pix.map((picture, i) => {
-        return <Card title={pix[i].title} url={pix[i].url} explanation={pix[i].explanation} date={pix[i].date} copyright={pix[i].copyright} />
+        return <Card key={pix[i].title} title={pix[i].title} url={pix[i].url} explanation={pix[i].explanation} date={pix[i].date} copyright={pix[i].copyright} />
     })
     return (
         <div className='images-container'>
