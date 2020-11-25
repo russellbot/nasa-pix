498699b9a76d0c6ef06148682ce548d99ee5efe2
Russell Hart <russellbot@gmail.com>
2020-11-24 21:00:27 -0500

    removed cardcomponent variable

--- src/CardList.js
+++ src/CardList.js
@@ -3,21 +3,22 @@ import Card from './Card';
 import './CardList.css'
 
 const CardList = ({ pix }) => {
-    const cardComponent = pix.map((picture, i) => {
-        return (
-            <Card 
-                key={pix[i].title} 
-                title={pix[i].title} 
-                url={pix[i].url} 
-                explanation={pix[i].explanation} 
-                date={pix[i].date} 
-                copyright={pix[i].copyright} 
-            />
-        )
-    })
     return (
         <div className='images-container'>
-            {cardComponent}
+            {
+                pix.map((picture, i) => {
+                    return (
+                        <Card 
+                            key={pix[i].title} 
+                            title={pix[i].title} 
+                            url={pix[i].url} 
+                            explanation={pix[i].explanation} 
+                            date={pix[i].date} 
+                            copyright={pix[i].copyright} 
+                        />
+                    )
+                })
+            }
         </div>
     );
 }
