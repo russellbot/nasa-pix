0c0c974059737e8f69daf43e73f0ad1526380515
Russell Hart <russellbot@gmail.com>
2020-11-30 12:27:08 -0500

    navigation styles

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Navigation = () => {
     return (
         <div className="navigation-container">
-            <h1>Nasa Pictures</h1>
+            <span className="background"></span>
+            <span className="title"><h1>Nasa Pictures</h1></span>
             <span className="navigation-items" id="resultsNav">
                 <h3 className="clickable">Favorites</h3>
                 <h3>&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;</h3>
