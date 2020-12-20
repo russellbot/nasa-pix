79007f6bd98248d275fef60fe9184582d9388f34
Russell Hart <russellbot@gmail.com>
2020-12-19 19:28:25 -0500

    added redux-logger middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import { searchPictures } from './reducers';
 import 'tachyons';
 
-const store = createStore(searchPictures)
+const logger = createLogger();
+const store = createStore(searchPictures, applyMiddleware(logger))
 
 ReactDOM.render(
   <React.StrictMode>
