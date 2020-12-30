06c2d3aee1c6f90584fd03f0457932882bff067a
Russell Hart <russellbot@gmail.com>
2020-12-30 13:58:06 -0500

    delete unused code

--- src/containers/App.js
+++ src/containers/App.js
@@ -33,93 +33,11 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class App extends React.Component {
-    // constructor() {
-    //     super()
-    //     this.state = {
-    //         pictures: [],
-    //         favorites: [],
-    //         page: 'home',
-    //         isAdded: false,
-    //         searchfield: '',
-    //     }
-    // }
-
-    // HOOKS
-    // const [pictures, setPictures] = useState([]);
-    // const [favorites, setFavorites] = useState([]);
-    // const [page, setPage] = useState('home');
-    // const [isAdded, setIsAdded] = useState(false);
-    // const [searchfield, setSearchfield] = useState('');
 
     componentDidMount() {
         this.props.onRequestFavorites();
         this.props.onRequestPictures();                 
     }
-
-    // HOOKS
-    // useEffect(() => {
-    //     loadMorePictures();
-    //     loadFavorites();  
-    // },[])
-
-    // const onSearchChange = (event) => {
-    //     setSearchfield(event.target.value)
-    // }
-
-    // loadMorePictures = () => {
-    //     this.setState({ page: 'home' });
-    //     const count = 10;
-    //     const apiKey = 'DEMO_KEY';
-    //     const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
-
-    //     fetch(apiUrl)
-    //         .then(response => response.json())
-    //         .then(newPics => {this.setState({ pictures: newPics})});
-    // }
-
-    // loadFavorites = () => {
-    //     if(localStorage.getItem('nasaFavorites')) {
-    //         let newFavorites = JSON.parse(localStorage.getItem('nasaFavorites'));
-    //         this.setState({ favorites: newFavorites });
-    //     }
-    // }
-
-    // saveFavoritesLocalStorage = () => {
-    //     localStorage.setItem('nasaFavorites', JSON.stringify(this.state.favorites));
-    // }
-
-    // loadFavoritesPage = () => {
-    //     this.setState({ page: 'favorites' })
-    //     this.loadFavorites();
-    // }
-
-    // saveFavorite = (picture) => {
-    //     let newFavorites = this.state.favorites;
-    //     // check if picture already exists in favorites
-    //     if(!newFavorites.includes(picture.object)) {
-    //         // add picture to favorites array
-    //         newFavorites.push(picture.object)
-    //         this.setState({ favorites: newFavorites });
-    //         // Show save confirmation for 2 seconds
-    //         this.setState({ isAdded: true });
-    //         setTimeout(() => {
-    //             this.setState({ isAdded: false });
-    //         }, 2000);
-    //     }
-    //     // Set favorites in localStorage
-    //     localStorage.setItem('nasaFavorites', JSON.stringify(this.state.favorites));       
-    // }
-
-    // removeFavorite = (picture) => {
-    //     let newFavorites = this.state.favorites
-    //     let index = newFavorites.indexOf(picture.object);
-    //     if (index !== -1) {
-    //         newFavorites.splice(index, 1);
-    //         this.setState({ favorites: newFavorites });
-    //         this.saveFavoritesLocalStorage();
-    //         this.loadFavorites();
-    //     }        
-    // }
     
     render() {
         const { searchField, onSearchChange, pictures, favorites, isPending, changePageFavorites, isAdded, saveFavorite, deleteFavorite, page, onRequestPictures } = this.props;
